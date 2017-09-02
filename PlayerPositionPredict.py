73748280cd7405ac07b3cfed4c6a6b4a2fca94ab
Rohit Anil <rohitanil7121993@gmail.com>
2017-09-02 13:08:03 +0530

    Code Cleanup

--- PlayerPositionPredict.py
+++ PlayerPositionPredict.py
@@ -1,11 +1,14 @@
+#Script to predict a player's Club Position using GaussianNB.
+
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
-df=pd.read_csv('/Users/continuumlabs/Downloads/FullData.csv.zip')
-label=df["Club_Position"]
-cols=["Rating","Penalties","Volleys","GK_Positioning"]
-train=df[cols]
 
-x=df[cols].values
+df=pd.read_csv('/Users/continuumlabs/Downloads/FullData.csv.zip') #Load csv file
+label=df["Club_Position"] 
+cols=["Rating","Penalties","Volleys","GK_Positioning"]
+train=df[cols] #select specified columns.
+
+x=df[cols].values 
 y=label.values
 x=x[:100]
 y=y[:100]
